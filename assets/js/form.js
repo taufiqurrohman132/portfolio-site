efab46e874d4541f00403ddf4cfa02237c254b71
Taufiqur Rohman <taufiqurrohman132132@gmail.com>
2025-07-26 12:58:53 +0700

    send

--- assets/js/form.js
+++ assets/js/form.js
@@ -9,3 +9,26 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('contact-form');
+  if (form) {
+    form.addEventListener('submit', () => {
+      // Tampilkan loading
+      form.querySelector('.loading').classList.add('d-block');
+      form.querySelector('.error-message').classList.remove('d-block');
+      form.querySelector('.sent-message').classList.remove('d-block');
+
+      // Opsional: sembunyikan loading setelah 1 detik
+      setTimeout(() => {
+        form.querySelector('.loading').classList.remove('d-block');
+      }, 1000);
+    });
+
+    // Jika redirect ke #contact (berarti pesan sukses)
+    if (window.location.hash === '#contact') {
+      form.querySelector('.sent-message').classList.add('d-block');
+    }
+  }
+});
